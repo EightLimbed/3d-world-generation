d89cbe606a41511d5641616798a58021158a516b
Dryden <drydenellis@gmail.com>
2025-01-15 21:48:35 -0500

    Revert "Threading Start"
    This reverts commit 18b8c2aa0c044934b86eed0561f407f908ffe4c7.

--- World/World.gd
+++ World/World.gd
@@ -8,50 +8,76 @@ var world = preload("res://World/Resources/Eden.tres")
 #size of what is in view, in chunks (x,y,z), needs to be odd to avoid artifacts
 @export var render_distance : int = 8
 var chunk = preload("res://World/Chunk.tscn")
-var rendered_chunks : Array
-var rendered_chunk_positions : Array[Vector3]
-var chunks_thread : Thread
+var rendered_chunks : Array[Vector3]
+
+@onready var noise = FastNoiseLite.new()
 @onready var random = RandomNumberGenerator.new()
 @onready var player = get_parent().get_child(1)
+@onready var chunk_container = $ChunkContainer
 @onready var label = $CanvasLayer/Label
 
 func _ready() -> void:
-		chunks_thread = Thread.new()
-		chunks_thread.start(generate_world.bind(player.position))
+		noise.noise_type = FastNoiseLite.TYPE_PERLIN
+		noise.frequency = 0.005
+		noise.seed = world.seeded
+		for child in $ChunkContainer.get_children():
+			child.queue_free() 
+
+func get_block1(pos : Vector3):
+	var block = 6
+	if pos.y <= 0:
+		block = 0
+	return block
+
+#noise parameters
+func get_block(pos : Vector3):
+	var hills = noise.get_noise_2dv(Vector2(pos.x,pos.z))*20
+	hills*=abs(hills)
+	hills+=abs(hills)/1.5
+	var caves_top = noise.get_noise_2dv(Vector2(pos.x,pos.z)*Vector2(4,4))*30
+	var caves = noise.get_noise_3dv(pos*Vector3(4,4,4))*10
+	var block : int = 6
+	#stone
+	if hills >= pos.y:
+		block = 1
+	#grass
+	elif hills >= pos.y-1:
+		block = 0
+	#caves
+	if caves > 3.2-min(abs(pos.y/16), 3) and pos.y < hills+caves_top-6:
+		block = 6
+	return block
 
 func _process(_delta: float) -> void:
-	#label.text = "total chunks: " + str(chunk_container.get_child_count()) + "\nfps: " + str(Engine.get_frames_per_second())+ "\ncoordinates: " + str(round(player.position))+ "\nchunk: " + str(pos_to_chunk(player.position))
-	pass
+	label.text = "total chunks: " + str(chunk_container.get_child_count()) + "\nfps: " + str(Engine.get_frames_per_second())+ "\ncoordinates: " + str(round(player.position))+ "\nchunk: " + str(pos_to_chunk(player.position))
+	generate_world(Vector3.ZERO)
 
 func pos_to_chunk(pos):
 	return round(pos/chunk_size)
 
 func generate_world(pos : Vector3):
-	while true:
-		#fills render distance with chunks
-		for x in range(render_distance):
-			for y in range(render_distance):
-				for z in range(render_distance):
-					#centers position around targeted area
-					var updated_pos = (pos_to_chunk(pos)+Vector3(x,y,z)-Vector3(render_distance,render_distance,render_distance)/2)*chunk_size
-					#creates chunk at position
-					if not rendered_chunk_positions.has(updated_pos):
-						create_chunk(updated_pos)
-		#clears chunks
-		for child in rendered_chunks:
-			if longest_distance(pos_to_chunk(child.position-pos)) > render_distance:
-				child.call_deferred("queue_free")
-				rendered_chunks.erase(child)
-				rendered_chunk_positions.erase(child.position)
+	#fills render distance with chunks
+	for x in range(render_distance):
+		for y in range(render_distance):
+			for z in range(render_distance):
+				#centers position around targeted area
+				var updated_pos = (pos_to_chunk(pos)+Vector3(x,y,z)-Vector3(render_distance,render_distance,render_distance)/2)*chunk_size
+				#creates chunk at position
+				if not rendered_chunks.has(updated_pos):
+					create_chunk(updated_pos, 1)
+					rendered_chunks.append(updated_pos)
+	#clears chunks
+	for child in chunk_container.get_children():
+		if longest_distance(pos_to_chunk(child.position-pos)) > render_distance:
+			child.queue_free()
+			rendered_chunks.erase(child.position)
 
-func create_chunk(pos : Vector3):
+func create_chunk(pos : Vector3, layer : int):
 	var instance = chunk.instantiate()
 	instance.position = pos
-	instance.parent = self
+	chunk_container.add_child(instance)
 	instance.generate()
-	call_deferred("add_child",instance)
-	rendered_chunks.append(instance)
-	rendered_chunk_positions.append(pos)
+	rendered_chunks.append(pos)
 
 func longest_distance(vec3 : Vector3):
 	var longest = abs(vec3.x)
