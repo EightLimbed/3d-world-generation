2089f32fb67bd160ad762d3cfb60375a9397a22c
Octopedibus <149276423+EightLimbed@users.noreply.github.com>
2025-01-17 13:34:12 -0500

    Added Auto CPS

--- World/World.gd
+++ World/World.gd
@@ -12,7 +12,7 @@ var world = preload("res://World/Resources/Eden.tres")
 var chunk = preload("res://World/Chunk.tscn")
 var rendered_chunks : Array[Vector3]
 var generated : bool = true
-@export var chunks_per_frame : int = 1#render_distance ** 3
+@onready var chunks_per_frame : int = 0
 var chunks_this_frame : int = 0
 signal new_frame
 
@@ -29,21 +29,21 @@ func _ready() -> void:
 	for child in $ChunkContainer.get_children():
 		child.queue_free()
 
-func get_block(pos : Vector3):
+func get_block1(pos : Vector3):
 	var block = 0
 	if pos.y <= 0:
 		block = 1
 	return block
 
 #noise parameters
-func get_block1(pos : Vector3):
+func get_block(pos : Vector3):
 	var hills = noise.get_noise_2dv(Vector2(pos.x,pos.z))*20
 	hills*=abs(hills)
 	hills+=abs(hills)/1.5
-	var caves_top = noise.get_noise_2dv(Vector2(pos.x,pos.z)*Vector2(4,4))*30
-	var caves = noise.get_noise_3dv(pos*Vector3(4,4,4))*10
+	var caves_top = noise.get_noise_2dv(Vector2(pos.x,pos.z)*4)*30
+	var caves = noise.get_noise_3dv(pos*5)*10
 	var block : int = 0
-	if not (caves > 3.2-min(abs(pos.y/16), 3) and pos.y < hills+caves_top-6):
+	if not (caves > 3.5-min(abs(pos.y/25), 3) and pos.y < hills+caves_top-6):
 		#stone
 		if hills >= pos.y:
 			block = 2
@@ -54,23 +54,23 @@ func get_block1(pos : Vector3):
 	return block
 
 func _process(delta: float) -> void:
-	#lets chunk generation know it can continue
-	new_frame.emit()
-	#lowers chunks per frame whenever fps drops below target
-	if delta > 1/target_fps:
-		#chunks_per_frame -= 1
-		pass
-	#if old operation is done, starts new one
-	if generated:
-		create_chunks(player.position)
 	#if new chunks have been generated, remove unneeded ones
 	if chunks_this_frame > 0:
+		#lowers chunks per frame whenever fps drops below target
+		if delta > 1/target_fps and chunks_per_frame > 0:
+			chunks_per_frame -= 1
+		elif delta < 1/target_fps and chunks_per_frame < render_distance**3:
+			chunks_per_frame += 1
 		remove_chunks(player.position)
 	#reset allowed chunks
 	chunks_this_frame = 0
+	#lets chunk generation know it can continue
+	new_frame.emit()
+	#if old operation is done, starts new one
+	if generated:
+		create_chunks(player.position)
 	#display useful information
 	label.text = "total chunks: " + str(chunk_container.get_child_count()) + "\nfps: " + str(Engine.get_frames_per_second())+ "\ncoordinates: " + str(round(player.position))+ "\nchunk: " + str(pos_to_chunk(player.position))+ "\nchunks per frame: " + str(chunks_per_frame)
-	print(rendered_chunks.size())
 
 func pos_to_chunk(pos):
 	return round(pos/chunk_size)
@@ -78,9 +78,9 @@ func pos_to_chunk(pos):
 func remove_chunks(pos : Vector3):
 	#clears chunks
 	for child in chunk_container.get_children():
-		if longest_distance(pos_to_chunk(child.position)-pos_to_chunk(pos)) > render_distance:
+		if longest_distance(pos_to_chunk(child.position)-pos_to_chunk(pos)) > render_distance-1:
 			rendered_chunks.erase(child.position)
-			child.queue_free()
+			child.call_deferred("queue_free")
 
 func create_chunks(pos : Vector3):
 	#fills render distance with chunks
@@ -88,21 +88,21 @@ func create_chunks(pos : Vector3):
 	for x in range(render_distance):
 		for y in range(render_distance):
 			for z in range(render_distance):
-				chunks_this_frame += 1
-				if chunks_this_frame >= chunks_per_frame:
-					await new_frame
 				#centers position around targeted area
 				var updated_pos = (pos_to_chunk(pos)+Vector3(x,y,z)-center)*chunk_size
 				#creates chunk at position if chunk not already rendered
 				if not rendered_chunks.has(updated_pos):
+					chunks_this_frame += 1
 					create_chunk(updated_pos)
 					rendered_chunks.append(updated_pos)
+				if chunks_this_frame >= chunks_per_frame:
+					await new_frame
 	generated = true
 
 func create_chunk(pos : Vector3):
 	var instance = chunk.instantiate()
 	instance.position = pos
-	rendered_chunks.append(pos)
+	rendered_chunks.append(instance.position)
 	chunk_container.add_child(instance)
 	instance.generate()
 
